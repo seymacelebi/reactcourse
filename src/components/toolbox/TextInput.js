961bf735cc730fb08a6341999ca758c68901747a
Şeyma Çelebi <celebiseyma98@gmail.com>
2021-11-23 23:47:06 +0300

    textInput

--- src/components/toolbox/TextInput.js
+++ src/components/toolbox/TextInput.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React from "react";
 
-const TextInput = ({name, label, onChange, placeHolder, value, error}) => {
-    let wrapperClass = "form-group"
-    if (error&& error.length>0 ) {
-        wrapperClass +=" has-error"
-        
-    }
-}
+const TextInput = ({ name, label, onChange, placeHolder, value, error }) => {
+  let wrapperClass = "form-group";
+  if (error && error.length > 0) {
+    wrapperClass += " has-error";
+  }
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={name}></label>
+      <div className="fiels">
+        <input
+          type="text"
+          name={name}
+          className="form-control"
+          placeholder={placeHolder}
+          value={value}
+          onChange={onChange}
+        ></input>
+        {error&&<div className="alert alert-danger">{error}</div>}
+      </div>
+    </div>
+  );
+};
 export default TextInput;
